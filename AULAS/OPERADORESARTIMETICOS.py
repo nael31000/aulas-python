03565fa9b1cfa7f3f848b4c4a8fa02c683069951
nael31000 <nael31000@gmail.com>
2026-09-01 19:57:11 -0300

    conteudo de operadores arimeticos

---
+++ AULAS/OPERADORESARTIMETICOS.py
@@ -0,0 +1,23 @@
+#operadores matematicos
+numero1 = 10
+numero2 = 20
+
+#soma
+soma=numero1+numero2
+#subtracao
+subtracao=numero1-numero2
+
+#multiplicacao
+multiplicacao=numero1*numero2
+
+#divisao
+divisao=numero1/numero2
+
+resto= numero1 % numero2
+print("soma dos numeros: ",soma)
+print("subtracao dos numeros: ",subtracao)
+print("multiplicacao dos numeros: ",multiplicacao)
+print("divisao dos numeros: ",divisao)
+print("resto dos numeros: ",resto)
+
+
