4779cde2e9d492b61b995103e73afb472b97283f
nael31000 <nael31000@gmail.com>
2026-09-02 19:25:18 -0300

    resposta da atividade 2

--- ATIVIDADES/Ativdade2.py
+++ ATIVIDADES/Ativdade2.py
@@ -4,6 +4,4 @@ idade = int(input("Qual é sua idade? "))
 plano_de_saude = input("Tem plano de saúde (True ou False)? ")
 
 aceito = idade >= 18 and idade <= 65 and plano_de_saude == "True"
-
-# Print usando vírgulas para separar o que é texto do que é variável
 print("Seu nome é", nome, "você tem", idade, "anos. Tem plano?", plano_de_saude, "Você foi aceito?", aceito)
